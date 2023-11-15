60a27cbbde1bf29b34e9488eb852bafa5b2dbb93
gaurav07singh10 <114424057+gaurav07singh10@users.noreply.github.com>
2023-11-15 13:25:39 +0530

    added project files

---
+++ server.js
@@ -0,0 +1,24 @@
+// jshint esversion6:
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("Public"));
+// app.set("view engine", "html");
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile('Public/index.html' , { root : __dirname});
+})
+app.get('/logging', (req, res)=> {
+    res.sendFile('Public/login.html' , { root : __dirname});
+})
+app.get('/register', (req, res)=> {
+    res.sendFile('Public/register.html' , { root : __dirname});
+})
+
+app.listen(3000, () => {
+    console.log("app listening at port 3000")
+})
