62df3015c12a998c3511ff51d807c67bfebc1434
Mann14 <mannmittal622@gmail.com>
2023-11-16 23:45:47 +0530

    change

--- server.js
+++ server.js
@@ -5,21 +5,29 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("Public"));
+
 app.set("view engine", "ejs");
 app.set('views',path.join(__dirname,'views'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("Public"));
 
 app.get('/logging',function(req,res){
     return res.render("login",{title:"login"});
 })
+
 app.get('/',function(req,res){
     return res.render('index',{title:"E-court"});
 })
+
 app.get('/register',(req,res)=>{
     return res.render('register',{title:"register"});
 })
 
-app.listen(port, () => {
-    console.log("app listening at port:",port)
+app.listen(port, (err) => {
+    if(err){
+        console.log("Error generated:",err);
+        return;
+    }
+    console.log("app listening at port:",port);
 })
